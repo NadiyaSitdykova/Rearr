158abd9141d18fa97e2c0cc3c55ea3359d4fd48d
NadiyaSitdykova <rovenskasa@gmail.com>
2014-05-17 14:27:53 +0400

    add evaluation of assembly approach

--- parse_gff.py
+++ parse_gff.py
@@ -14,6 +14,24 @@ def median_parsegff(inputfile, d):
         d[geneID] = (seqID, sum(coordinates)/len(coordinates), direction)
 
 
+def process_gene(d, coordinates, seqID, geneID, direction):
+    consequtive = True
+    start = min(coordinates[0])
+    end = max(coordinates[1])
+    nonconsequtive = []
+    for k, v in d.items():
+        v_seqID, v_start, v_end, v_dir = v
+        if seqID == v_seqID:
+            if start > v_start and end < v_end:
+                nonconsequtive.append(k)
+            elif start > v_start and start < v_end or end > v_start and end < v_end:
+                nonconsequtive.append(k)
+                consequtive = False
+    for k in nonconsequtive:
+        d.pop(k, None)
+    if consequtive:
+        d[geneID] = (seqID, start, end, direction)
+
 def consequtive_parsegff(inputfile, d):
     with open(inputfile, 'r') as file:
         geneID, seqID, direction, coordinates, last = "trash", "", "", [[0],[0]], ""
@@ -23,34 +41,13 @@ def consequtive_parsegff(inputfile, d):
                 coordinates[0].append(int(blocks[3]))
                 coordinates[1].append(int(blocks[4]))
             else:
-                consequtive = True
-                start = min(coordinates[0])
-                end = max(coordinates[1])
-                nonconsequtive = []
-                for k, v in d.items():
-                    _, v_start, v_end, v_dir = v
-                    if start > v_start and end < v_end:
-                        nonconsequtive.append(k)
-                    elif start > v_start and start < v_end or end > v_start and end < v_end:
-                        nonconsequtive.append(k)
-                        consequtive = False
-                for k in nonconsequtive:
-                    d.pop(k, None)
-                if consequtive:
-                    d[geneID] = (seqID, start, end, direction)
+                process_gene(d, coordinates, seqID, geneID, direction)
                 geneID = blocks[10].strip()
                 seqID = blocks[0].strip()
                 coordinates = [[int(blocks[3])], [int(blocks[4])]]
                 direction = blocks[6].strip()
             last = blocks[10].strip()
-
-        consequtive = True
-        start = min(coordinates[0])
-        end = max(coordinates[1])
-        for k, v in d.items():
-            v_seq, v_start, v_end, v_dir = v
-            if start > v_start and start < v_end or end > v_start and end < v_end:
-                consequtive = False
-            d[k] =(v_seq, v_start, v_dir)
-        if consequtive:
-                    d[geneID] = (seqID, start, direction)
+        process_gene(d, coordinates, seqID, geneID, direction)
+    for k, v in d.items():
+        v_seqId, v_start, _, v_dir = v
+        d[k] = (v_seqId, v_start, v_dir)
