b8253c7d7ed36d302eebc41cfeebb30f0255969d
NadiyaSitdykova <rovenskasa@gmail.com>
2015-04-24 17:30:35 +0300

    add some comments

--- prepare_grimm_with_repeats/prepare_grimm.py
+++ prepare_grimm_with_repeats/prepare_grimm.py
@@ -1,4 +1,3 @@
-import sys
 import node
 
 class Gene_info(object):
@@ -31,15 +30,11 @@ def read_repeats_file(filename, repeat_to_label, genomes, genome_name):
             if len(blocks) > 0 and blocks[0] != "SW" and blocks[0] != "score":
                 repeat_to_label[blocks[9]] = 0
                 if blocks[4][3:] not in genomes[genome_name]: # chr1, chrX format of chromosome name
-                    #print(blocks[4])
-                    #genomes[genome_name][blocks[4][3:]] = []
                     continue
                 strand = 1
                 if blocks[8] == 'C':
                     strand = -1
                 genomes[genome_name][blocks[4][3:]].append(Gene_info(blocks[9], int(blocks[5]), int(blocks[6]), strand, True))
-
-
 
 def process_paralogs(filename, nodes, gene_to_num):
     with open(filename, 'r') as file:
@@ -74,13 +69,15 @@ if __name__ == '__main__':
     gene_data_files = ["data/diverse_vertebrate_dataset/Human/gene_data.txt", "data/diverse_vertebrate_dataset/Chimp/gene_data.txt", "data/diverse_vertebrate_dataset/Mouse/gene_data.txt", "data/diverse_vertebrate_dataset/Rat/gene_data.txt", "data/diverse_vertebrate_dataset/Dog/gene_data.txt", "data/diverse_vertebrate_dataset/Cat/gene_data.txt", "data/diverse_vertebrate_dataset/Opossum/gene_data.txt"]
     paralogs_files = ["data/diverse_vertebrate_dataset/Human/paralogs.txt", "data/diverse_vertebrate_dataset/Chimp/paralogs.txt", "data/diverse_vertebrate_dataset/Mouse/paralogs.txt", "data/diverse_vertebrate_dataset/Rat/paralogs.txt", "data/diverse_vertebrate_dataset/Dog/paralogs.txt", "data/diverse_vertebrate_dataset/Cat/paralogs.txt", "data/diverse_vertebrate_dataset/Opossum/paralogs.txt"]
     orthologs_files = ["data/diverse_vertebrate_dataset/Human/orthologs.txt", "data/diverse_vertebrate_dataset/Chimp/orthologs.txt", "data/diverse_vertebrate_dataset/Mouse/orthologs.txt", "data/diverse_vertebrate_dataset/Rat/orthologs.txt", "data/diverse_vertebrate_dataset/Dog/orthologs.txt", "data/diverse_vertebrate_dataset/Cat/orthologs.txt", "data/diverse_vertebrate_dataset/Opossum/orthologs.txt"]
-    repeats_files = ["data/diverse_vertebrate_dataset/Human/300_hg38.fa.out", "data/diverse_vertebrate_dataset/Chimp/300_panTro4.fa.out", "data/diverse_vertebrate_dataset/Mouse/300_mm10.fa.out", "data/diverse_vertebrate_dataset/Rat/300_rn5.fa.out", "data/diverse_vertebrate_dataset/Dog/300_canFam3.fa.out", "data/diverse_vertebrate_dataset/Cat/300_felCat5.fa.out", "data/diverse_vertebrate_dataset/Opossum/300_monDom5.fa.out"]
+    repeats_files = ["data/diverse_vertebrate_dataset/Human/400_hg38.fa.out", "data/diverse_vertebrate_dataset/Chimp/400_panTro4.fa.out", "data/diverse_vertebrate_dataset/Mouse/400_mm10.fa.out", "data/diverse_vertebrate_dataset/Rat/400_rn5.fa.out", "data/diverse_vertebrate_dataset/Dog/400_canFam3.fa.out", "data/diverse_vertebrate_dataset/Cat/400_felCat5.fa.out", "data/diverse_vertebrate_dataset/Opossum/400_monDom5.fa.out"]
     nodes = []
 
+    #read gene_data and repeatmasker's files
     for i in range(0, len(names)):
         read_gene_data_file(gene_data_files[i], gene_to_label, genomes, names[i])
         read_repeats_file(repeats_files[i], repeats, genomes, names[i])
 
+    #ininialize DSU of genes
     i = 0
     gene_to_num = {}
     for k, v in gene_to_label.items():
@@ -88,17 +85,18 @@ if __name__ == '__main__':
         gene_to_num[k] = i
         i += 1
 
+    #join some genes in DSU using info from files of paralogs and orthologs
     for i in range(0, len(names)):
-        print(names[i])
         process_paralogs(paralogs_files[i], nodes, gene_to_num)
         process_orthologs(orthologs_files[i], nodes, gene_to_num)
 
+    #label repeats
     cur_label = 1
     for repeat, v in repeats.items():
         repeat_to_label[repeat] = cur_label
         cur_label += 1
-    #print(len(repeat_to_label))
 
+    #label genes using info from DSU
     processed = {}
     cur_label = 1
     for i in range(0, len(nodes)):
@@ -109,16 +107,15 @@ if __name__ == '__main__':
                 gene_to_label[gene] = cur_label
             cur_label += 1
 
-    #print(len(gene_to_num))
-    #print(cur_label)
-
+    #sort genes inside chromosomes
     for genome, chromes in genomes.items():
         for chrome, genes in chromes.items():
             genes.sort(key=lambda x: x.mid)
 
+    #save grimm file
     with open("out.txt", 'w') as out:
         for genome, chromes in genomes.items():
-            out.write(">" + genome)
+            out.write(">" + genome + "\n")
             for chrome, genes in chromes.items():
                 s = ""
                 for gene in genes:
@@ -126,7 +123,7 @@ if __name__ == '__main__':
                         s += str(gene.strand * repeat_to_label[gene.id]) + "_repeat "
                     else:
                         s += str(gene.strand * gene_to_label[gene.id]) + " "
-                s += "$"
+                s += "$\n"
                 out.write(s)
 
 
