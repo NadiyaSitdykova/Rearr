efb26ada05c3ade81cc8a6d39c59c16a7130624f
NadiyaSitdykova <rovenskasa@gmail.com>
2015-05-04 17:20:24 +0300

    add identity parameter for filtration

--- prepare_grimm_with_repeats/filter_repeats.py
+++ prepare_grimm_with_repeats/filter_repeats.py
@@ -5,14 +5,16 @@ twice = {}
 
 if __name__ == '__main__':
     filename = sys.argv[1]
-    threshold = int(sys.argv[2])
+    threshold_length = int(sys.argv[2])
+    threshold_identity = float(sys.argv[3])
     with open(filename, 'r') as file:
         for line in file.readlines():
             blocks = line.split()
             if len(blocks) > 0 and blocks[0] != "SW" and blocks[0] != "score":
                 repeat_class = blocks[10].split("/")[0]
                 repeat_length = int(blocks[6]) - int(blocks[5])
-                if repeat_class != "DNA" or repeat_length <= threshold:
+                repeat_identity = 100 - (float(blocks[1]) + float(blocks[2]) + float(blocks[3]))
+                if repeat_class != "DNA" or repeat_length < threshold_length or repeat_identity < threshold_identity:
                     continue
                 repeat = blocks[9]
                 if repeat in once:
@@ -21,12 +23,14 @@ if __name__ == '__main__':
                     once[repeat] = True
             lines.append(line)
 
-    with open(str(threshold) + "_" + filename, 'w') as out:
+    with open("filtered_" + filename, 'w') as out:
         for line in lines:
             blocks = line.split()
             if len(blocks) > 0 and blocks[0] != "SW" and blocks[0] != "score":
                 repeat = blocks[9]
                 if repeat in twice:
                     out.write(line)
+
+
 
 
